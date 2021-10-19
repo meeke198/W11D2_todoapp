ae561073b1cbf969d855b2d678975d0284ec872c
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-19 14:45:56 -0400

    Receiving and Reducing todos

--- todos/frontend/reducers/todo_reducers.js
+++ todos/frontend/reducers/todo_reducers.js
@@ -1,7 +1,34 @@
 //Redux reducers manage the shape of your application state. 
 //returns the next state tree, given the current state tree and an action to handle
-const todosReducer = (state = {}, action) => {
+
+import { RECEIVE_TODO, RECEIVE_TODOS } from "../actions/todo_actions";
+
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
+    Object.freeze(state); //A frozen object can no longer be changed; 
+    let nextState = Object.assign({}, state); //make a copy of the original state
   switch (action.type) {
+      case RECEIVE_TODO:
+        nextState[action.todo.id] = action.todo; // assigns a key of the tea's id to the value of the entire tea
+      return nextState;
+      case RECEIVE_TODOS:
+           nextState[action.todos] = action.todos; // assigns a key of the tea's id to the value of the entire tea
+      return nextState;
     default:
       return state;
   }
