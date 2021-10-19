806c1f4a9b681a324cbf00ea7bee323b5a8d0b39
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-19 14:26:24 -0400

    redux store

--- todos/todo_redux.jsx
+++ todos/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configureStore } from './frontend/store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    window.store = store;
     ReactDOM.render(<h1>Todos App</h1>, document.getElementById("root"))
 });
