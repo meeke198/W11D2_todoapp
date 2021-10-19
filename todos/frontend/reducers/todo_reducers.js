2c1413421a2a1ee5f733aa084afd415e7ebb7f71
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-19 14:08:34 -0400

    root_reducer

---
+++ todos/frontend/reducers/todo_reducers.js
@@ -0,0 +1,10 @@
+//Redux reducers manage the shape of your application state. 
+//returns the next state tree, given the current state tree and an action to handle
+const todosReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
