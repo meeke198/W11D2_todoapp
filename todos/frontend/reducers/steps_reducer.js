4646fee69d553a3e58d785d26440f18be638d1c5
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-20 11:56:04 -0400

    add Step reducuer

--- todos/frontend/reducers/steps_reducer.js
+++ todos/frontend/reducers/steps_reducer.js
@@ -5,15 +5,15 @@ const stepsReducer = (state = {}, action) => {
     let nextState = Object.assign({}, state); //make a copy of the original state
     switch (action.type) {
         case RECEIVE_STEP:
-            nextState[action.todo.id] = action.todo;
+            nextState[action.step.id] = action.step;
             return nextState;
         case RECEIVE_STEPS:
-            for (let i = 0; i < action.todos.length; i++){
-                nextState[action.todos[i].id] = action.todos[i];
+            for (let i = 0; i < action.steps.length; i++){
+                nextState[action.steps[i].id] = action.steps[i];
             }
             return nextState;
         case REMOVE_STEP:
-            delete nextState[action.todo.id];
+            delete nextState[action.step.id];
             return nextState;
         default:
             return state;
