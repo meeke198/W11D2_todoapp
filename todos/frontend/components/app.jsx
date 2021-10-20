6d2bba7bb0dd0b6b57305ffd0f336a0fac6d5a96
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-20 15:46:55 -0400

    build TodoListContainer

--- todos/frontend/components/app.jsx
+++ todos/frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import TodoListContainer from "./todos/todo_list_container";
 
 const App = () => (
     <div>
         <h1>Todo App</h1>
+        <TodoListContainer />
     </div>
 )
 
---
+++ todos/frontend/components/todos/todo_list_container.jsx
@@ -0,0 +1,15 @@
+
+import { connect } from 'react-redux';
+import allTodos from '../../reducers/selectors';
+import TodoList from './todo_list';
+
+
+const mapStateToProps = state => ({
+    todos: allTodos(state)      //todos: prop
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  receiveTodo: todo => dispatch(receiveTodo(todo)) //receiveTodo: prop
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
